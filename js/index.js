b916d6ae11c6626e5ea22705ab84e393dccf9976
BAtkins <Benjamin.Atkins@ruthmann.de>
2021-07-06 14:35:51 +0200

    - Code-Eingabe für Schülerbereich bearbeitet: - Die maximale mögliche Zeichenmenge pro Codeblock wurde auf 2 begrenzt - Es können nur noch Ziffern zwischen 0 und 9 eingegeben werden - Andere Zeichen werden entfernt - FIXME Es kann immer noch eine Kombination aus beidem eingegeben werden   - Fehler wird korrigiert

--- js/index.js
+++ js/index.js
@@ -13,6 +13,35 @@ gui.functionMannager = functionMannager;
 
 gui.loadSvgAndMenus("index");
 
+document.addEventListener("mousedown", ()=>{
+    if (document.getElementById("input_area_students") != undefined);
+    {
+        var elements = document.getElementsByClassName("input_student");
+        console.log(elements);
+        if (elements.length != 0)
+        {
+            for(let i = 0; i < elements.length; i++)
+            {
+                elements[i].addEventListener("input", ()=>{
+                    var charArray = elements[i].value.split(',');
+                    if(charArray.length > 0)
+                    {
+                        for(let j = 0; j < charArray.length; j++)
+                        {
+                            if(charArray[j] < '0' || charArray[j] > '9' )
+                            {
+                                charArray[j] = "";
+                                elements[i].value = charArray;
+                                console.log("Es dürfen nur Zahlen eingegeen werden!");
+                            }
+                        }
+                    }
+                })
+            }
+        }
+    }   
+})
+
 
 
 
