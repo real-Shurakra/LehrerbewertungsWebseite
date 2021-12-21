6d26daf3361b844c9595ea7cb640842b54e3931c
Matthias Koperski <matthias.koperski@hotmail.de>
2021-12-21 10:57:30 +0100

    moved Bootstrap & cunstructing AdminPage

---
+++ AdminPage/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/bootstrap4/bootstrap.min.css">
+        <script src="js/bootstrap4/jquery.min.js"></script>
+        <script src="js/bootstrap4/popper.min.js"></script>
+        <script src="js/bootstrap4/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="jumbotron">
+            <h1>Hello World</h1>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-12">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ AdminPage/js/language.js
@@ -0,0 +1,5 @@
+class language{
+    setLanguage(){
+        
+    }
+}
